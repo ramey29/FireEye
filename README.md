dd7c4a4d571aab068bd92af42d2fb4530dea785b
Rameshwar Bishnoi <vishnoirameshwar@gmail.com>
2017-08-16 18:33:43 +0530

    Added new way to call API (Tech Radar ).
    New API call and way to push tweet to User 2 Timeline.

--- README.md
+++ README.md
@@ -14,3 +14,5 @@ Have used Twitter widget to get the two peoples timeline.You can tweet to one us
 Apart from that the project has capabilities of Browser Sync, concatenation, minification of js, minification of css, image minification and run sequence which will take care of all your project development needs dynamically.
 And at the end there will be 'dist' folder created which could be used to deploy on any sever.
 
+I have added to API (both are Tech Radar) as User 1 and User 2 and way to add Tweet to User 2 , Though it adds things like Tweet dynamically , The username and image is static.
+
--- js/main.js
+++ js/main.js
@@ -9,14 +9,56 @@ this.getTechRadardata = function(){
    return response;
    
  })
-}
-}]);
-
-myApp.controller('techRadar',['$scope', 'techRadarService', function($scope, techRadarService){
-techRadarService.getTechRadardata().then(function(response){
- console.log(response.data.articles)
- $scope.radarGroup = response.data;
- $scope.articles = response.data.articles;
-})
+};
 
 }]);
+
+myApp.service('nYTimesService',['$http',function($http){
+this.getNYTimesdata = function(){
+	
+ return $http.get('https://newsapi.org/v1/articles?source=techradar&sortBy=latest&apiKey=b6e6c046741e4ebcac0307c897f39da6')
+ .then(function(response){
+   return response;
+   
+ })
+};
+
+}]);
+
+myApp.controller('techRadar',['$scope', 'techRadarService', 'nYTimesService' , function($scope, techRadarService, nYTimesService){
+	$scope.articles = [];
+	$scope.api1 = function(){
+		 techRadarService.getTechRadardata().then(function(response){
+		 
+		 $scope.radarGroup = response.data;
+		 $scope.articles = response.data.articles;
+		});
+};
+$scope.api1();
+$scope.api2 = function(){
+		nYTimesService.getNYTimesdata().then(function(response){
+		 console.log(response.data)
+		 $scope.NYGroup = response.data;
+		 $scope.articles = response.data.articles;
+		});
+		
+
+$scope.addTweet = function () {
+
+        $scope.articles.push({
+            author: 'RameshwarBishnoi',
+            title: $scope.tweet,
+			publishedAt: '2017-08-16T11:11:00.227Z',
+			urlToImage:"http://www.gratisography.com/pictures/417_1.jpg"
+        });
+
+        // Clear input fields after push
+        $scope.tweet = "";
+       
+
+    };
+};
+
+
+}]);
+
